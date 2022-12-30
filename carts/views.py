a30f20f7d9fe0e0679e2777bdfa080420234d685
campeone-pixel <posesmatiasabel@gmail.com>
2022-12-30 12:05:55 -0300

    i finished the funtion for check if the item was added to car

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cart, CartItem
 from store.models import Product
-from django.http import HttpResponse
+
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def _cart_id(request):
@@ -9,6 +10,25 @@ def _cart_id(request):
     if not cart:
         cart = request.session.create()
     return cart
+
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    product = get_object_or_404(Product, id = product_id)
+    cart_item = CartItem.objects.get(product = product, cart = cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity-= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    product = get_object_or_404(Product, id = product_id)
+    cart_item = CartItem.objects.get(product = product , cart=cart)
+    cart_item.delete()
+    return redirect('cart')
+
 
 
 def add_cart(request, product_id):
@@ -27,15 +47,33 @@ def add_cart(request, product_id):
         cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
 
     cart_item.save()
-    return HttpResponse (cart_item.quantity)
-    # return redirect("cart")
+    
+    return redirect("cart")
 
 
 def cart(request,total=0,quantity=0, car_items=None):
     try:
-        cart = Cart.objects.get(car_id=_cart_id(request))
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart = cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total + tax
 
-    except:
+    except ObjectDoesNotExist:
+
         pass
 
-    return render(request, "store/cart.html")
+    context={
+        'total':total,
+        'quantity':quantity,
+        'cart_items':cart_items,
+        'tax': tax,
+        'grand_total': grand_total,
+    }
+
+    return render(request, "store/cart.html",context)
+
+
+
